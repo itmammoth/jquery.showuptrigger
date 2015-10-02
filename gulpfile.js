e807f5955091e1291ed93f405af5c7e8fe5cadb8
itmammoth <itmammoth@gmail.com>
2015-10-02 15:14:37 +0700

    Introduced gulp.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var browser = require('browser-sync');
+
+gulp.task('default', ['server'], function() {
+  gulp.watch(['js/**/*.js', '!js/min/**/*.js'], ['js']);
+  gulp.watch(['*.html'], browser.reload);
+});
+
+gulp.task('js', function() {
+  gulp.src(['js/**/*.js', '!js/min/**/*.js'])
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('./js/min'))
+      .pipe(browser.reload({ stream: true }))
+});
+
+gulp.task('server', function() {
+  browser({
+    server: {
+      baseDir: './',
+      index: 'test.html'
+    }
+  });
+});
